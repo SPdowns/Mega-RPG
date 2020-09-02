e8698726dcf1a760cb63b21311c0ceefc2bbfb74
Micheal Hansen <micheal.hansen@gmail.com>
2020-09-02 15:39:01 -0700

    add character creation logic to main.js
    Co-authored-by: SPdowns <spdowns@protonmail.com>

--- src/main.js
+++ src/main.js
@@ -2,7 +2,34 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import { characterCreate } from './character';
+import { addAttribute } from './state';
+
+const updateCharacterDisplay = (character) => {
+  $("#character-name").html(character.name);
+  $("#health-display").html(character.health);
+  $("#strength-display").html(character.strength);
+  $("#toughness-display").html(character.toughness);
+  $("#intelligence-display").html(character.intelligence);
+};
 
 $(document).ready(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
 
+    const name = $("input#input-name").val();
+    const strength = parseInt($("input#input-strength").val());
+    const toughness = parseInt($("input#input-toughness").val());
+    const intelligence = parseInt($("input#input-intelligence").val());
+
+    const character = characterCreate(name);
+    character(addAttribute("strength")(strength));
+    character(addAttribute("toughness")(toughness));
+    character(addAttribute("intelligence")(intelligence));
+    character(addAttribute("health")(toughness));
+
+    updateCharacterDisplay(character);
+    $("#character-creation").toggle();
+    $("#battle-display").toggle();
+  });
 });
