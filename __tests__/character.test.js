7fbf19644fbe02c0ccedeba90fca2afacd90f3a6
Micheal Hansen <micheal.hansen@gmail.com>
2020-09-02 10:58:25 -0700

    add test and code for assigning attributes
    Co-authored-by: SPdowns <spdowns@protonmail.com>

--- __tests__/character.test.js
+++ __tests__/character.test.js
@@ -1,9 +1,23 @@
 /* eslint-disable no-undef */
-import { characterCreate } from './../src/character';
+import { characterCreate, addAttribute } from './../src/character';
 
 describe("character creation", ()=>{
   test("should be able to create a character", ()=>{
     const newCharacter = characterCreate("Sudo");
     expect(newCharacter().name).toBe("Sudo");
+  });
+
+  test("should be able to add attributes to a character", ()=>{
+    const newCharacter = characterCreate("Sudo");
+    newCharacter(addAttribute("strength")(10));
+    expect(newCharacter().strength).toBe(10);
+  });
+
+  test("should be able to recycle the addAttribute function", ()=>{
+    const newCharacter = characterCreate("Sudo");
+    newCharacter(addAttribute("strength")(10));
+    newCharacter(addAttribute("intelligence")(100000));
+    expect(newCharacter().strength).toBe(10);
+    expect(newCharacter().intelligence).toBe(100000);
   });
 });
--- src/character.js
+++ src/character.js
@@ -1,4 +1,4 @@
-import { storeState } from './state';
+import { storeState, changeState } from './state';
 
 export const characterCreate = (name) => { //characterCreate("Sudo")
   let state = {
@@ -11,3 +11,16 @@ export const characterCreate = (name) => { //characterCreate("Sudo")
 // character() => return the current state of "Sudo";
 // character(strength(1)) => increase "Sudo" strength by 1;
 // const character2 = characterCreate("Sean");
+
+export const addAttribute = (attributeName) => {
+  return (attributeValue) => {
+    return changeState(attributeName)(attributeValue);
+  };
+};
+//character(addAttribute("strength")(10))
+
+// export const addStrength = (value) => {
+//   return changeState("strength")(value);
+// };
+
+//character(addStrength(10));
